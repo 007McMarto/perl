9c77f9c0e327e2f610516f77ce6ee280953a919f
007McMarto <nigol@posteo.net>
2018-05-31 08:38:37 +0200

    another file

---
+++ poly.pl
@@ -0,0 +1,40 @@
+#!/usr/bin/perl
+
+use strict;
+use warnings;
+
+sub arrayPoly {
+	
+	my @input = @_;
+	print "Size of array: " . ($#input + 1) . "\n"; 
+	my $term = '';
+	for (my $i=$#input; $i >= 0; $i--) {
+		$term = $term . " + " . $input[$i] . "x" . $i;
+	}
+
+	return $term;
+}
+
+
+sub hashPoly {
+
+	my %hash = @_;
+	my $term = '';
+	for my $i (keys %hash) {
+		$term = $term . " + " . $hash{$i} . "x" . $i;
+	}
+	return $term;
+}
+
+my @testArray = (5,7,2);
+print "Initial array has size " . ($#testArray + 1) . "\n";
+print arrayPoly(@testArray) . "\n";
+
+my %testHash = ( 
+	2 => 2,
+	0 => 5,
+	1 => 7
+);
+
+print hashPoly(%testHash) . "\n";
+
